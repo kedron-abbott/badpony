81d42b983530024a7b916d4db474eb6eac3d050a
kedronabbott <kedron@nine.is>
2017-04-14 19:12:38 -0500

    adding everything so far

---
+++ static/badPony-Scripts.js
@@ -0,0 +1,20 @@
+function changeMenu(x) {
+    x.classList.toggle("change");
+    document.getElementById("mySidenav").classList.toggle("navOpen");
+    document.getElementById("everything").classList.toggle("navOpen");
+}
+
+
+/*var w = 1;
+function changeMenu(x) {
+    w = w +1;
+    x.classList.toggle("change");
+    document.getElementById("mySidenav").classList.toggle("navOpen");
+    document.getElementById("everything").classList.toggle("navOpen");
+
+  if (w%2 == 0){
+    document.getElementById("body").style.marginLeft = "-250px";}
+  else{
+    document.getElementById("body").style.marginLeft = "0px";}
+}
+*/
