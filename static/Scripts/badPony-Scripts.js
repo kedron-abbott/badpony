16b28b3a929fc35c7c8b231a9a6b9f5f4062d837
kedronabbott <kedron@nine.is>
2017-04-27 17:30:46 -0500

    fixed parallax and testimonial sizing

--- static/Scripts/badPony-Scripts.js
+++ static/Scripts/badPony-Scripts.js
@@ -3,3 +3,13 @@ function changeMenu(x) {
     document.getElementById("mySidenav").classList.toggle("navOpen");
     document.getElementById("everything").classList.toggle("navOpen");
 }
+
+$(document).ready(function() {
+    var maxHeight = 0;
+    $('.testimonialSelect').each(function() {
+      var divHeight = $(this).height();
+      if (maxHeight == 0 || maxHeight < divHeight) maxHeight = divHeight;
+    });
+
+    $('.testimonialSelect').height(maxHeight);
+});
